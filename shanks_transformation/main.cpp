a8037cfc660e1fe0763627f4ace67669a57a080b
boryaa <pashkovborya@gmail.com>
2023-10-16 21:55:07 +0300

    added ch(x) series. updated factorial function

--- shanks_transformation/main.cpp
+++ shanks_transformation/main.cpp
@@ -2,7 +2,15 @@
 
 int fact(int n)
 {
-	return n > 0 ? fact(n - 1) * n : 1;
+	int f = 1;
+	if (n > 0)
+	{
+		for (int i = 2; i <= n; i++)
+		{
+			f *= i;
+		}
+	}
+	return f;
 }
 
 float exp_x(float x, int n)
@@ -15,13 +23,21 @@ float four_arctan_x(float x, int n)
 	return 4*pow((-1), n % 2) * pow(x, 2 * n + 1) / (2 * n + 1);
 }
 
+float ch_x(float x, int n)
+{
+	return pow(x, 2 * n) / fact(2 * n);
+}
+
 int main(void)
 {
 	shanks_transform<float> test(&exp_x, 2.5);
 	test.print_t_n(5);
 	test.print_s_n(5);
-	shanks_transform<float> test2(&four_arctan_x, 1);
-	test2.print_t_n(5);
-	test2.print_s_n(5);
+	shanks_transform<float> test1(&four_arctan_x, 1);
+	test1.print_t_n(5);
+	test1.print_s_n(5);
+	shanks_transform<float> test2(&ch_x, 2.00001);
+	test2.print_t_n(3);
+	test2.print_s_n(3);
 	return 0;
 }
