060044df744824e0f3327e5fd964c2842868a2bc
MikyMikyMiky <mike1024b@mail.ru>
2024-01-24 09:22:34 +0000

    Deploying to gh-pages from @ katerina-Evdokimova/shanks-university@c74b95c575e5a48168853e0076a1de6b34fe42f5 🚀

--- search/all_d.js
+++ search/all_d.js
@@ -3,7 +3,7 @@ var searchData=
   ['s_5fn_29',['S_n',['../classseries__base.html#acaff7c9f11abe62fd6f8ac622578025d',1,'series_base']]],
   ['series_30',['series',['../classseries__acceleration.html#a79e096bcc3434265a9c99522a41ff820',1,'series_acceleration']]],
   ['series_2eh_31',['series.h',['../series_8h.html',1,'']]],
-  ['series_5facceleration_32',['series_acceleration',['../classseries__acceleration.html',1,'series_acceleration&lt; T, K, series_templ &gt;'],['../classseries__acceleration.html#a1df90a926913831d683a8845a45bdbab',1,'series_acceleration::series_acceleration(const series_templ &amp;series)'],['../classseries__acceleration.html#a8e5b786cd4f81b64b730b4c47843b046',1,'series_acceleration::series_acceleration()']]],
+  ['series_5facceleration_32',['series_acceleration',['../classseries__acceleration.html',1,'series_acceleration&lt; T, K, series_templ &gt;'],['../classseries__acceleration.html#a1df90a926913831d683a8845a45bdbab',1,'series_acceleration::series_acceleration()']]],
   ['series_5facceleration_2eh_33',['series_acceleration.h',['../series__acceleration_8h.html',1,'']]],
   ['series_5fbase_34',['series_base',['../classseries__base.html',1,'series_base&lt; T, K &gt;'],['../classseries__base.html#a42b6c38ba4fb197990696a62ff2c949c',1,'series_base::series_base(T x, T sum)'],['../classseries__base.html#aeb803a28007292df232c62c1311bfc3d',1,'series_base::series_base(T x)'],['../classseries__base.html#aaa1aff44735f74d0ca15f0559699970e',1,'series_base::series_base()']]],
   ['shanks_5ftransform_35',['shanks_transform',['../classshanks__transform.html',1,'shanks_transform&lt; T, K, series_templ &gt;'],['../classshanks__transform.html#a7895e7732ab2b125ab53e9244b478508',1,'shanks_transform::shanks_transform()']]],
