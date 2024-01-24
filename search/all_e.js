a8bec05eaa6e8b5754f9df0dfb7c72fc0544bd12
MikyMikyMiky <mike1024b@mail.ru>
2024-01-24 08:46:06 +0000

    Deploying to gh-pages from @ katerina-Evdokimova/shanks-university@b8c58d5c27f1af3e4b96d54034c029274e39964b 🚀

--- search/all_e.js
+++ search/all_e.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['test_5ffunctions_2eh_39',['test_functions.h',['../test__functions_8h.html',1,'']]],
-  ['transformation_5fremainders_40',['transformation_remainders',['../test__functions_8h.html#ab6ba403fc3fb51ee8ad8a2a7eb52bda5',1,'test_functions.h']]]
+  ['test_5ffunctions_2eh_40',['test_functions.h',['../test__functions_8h.html',1,'']]],
+  ['transformation_5fremainders_41',['transformation_remainders',['../test__functions_8h.html#ab6ba403fc3fb51ee8ad8a2a7eb52bda5',1,'test_functions.h']]]
 ];
--- search/functions_8.js
+++ search/functions_8.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['mean_5fsinh_5fsin_5fseries_96',['mean_sinh_sin_series',['../classmean__sinh__sin__series.html#ae02295e25910fb37a6c2734aa40a0538',1,'mean_sinh_sin_series::mean_sinh_sin_series()'],['../classmean__sinh__sin__series.html#affb3220de11c00a141c623f081b982fe',1,'mean_sinh_sin_series::mean_sinh_sin_series(T x)']]]
+  ['mean_5fsinh_5fsin_5fseries_86',['mean_sinh_sin_series',['../classmean__sinh__sin__series.html#affb3220de11c00a141c623f081b982fe',1,'mean_sinh_sin_series']]]
 ];
