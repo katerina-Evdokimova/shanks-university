f15b795667d6e19b4c202f61f05bcca41e5a47f6
MikyMikyMiky <mike1024b@mail.ru>
2024-02-10 19:35:17 +0000

    Deploying to gh-pages from @ katerina-Evdokimova/shanks-university@9d27077a9d4438d24e88762f0b38934e07b432ec 🚀

--- search/all_1.js
+++ search/all_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['bin_5fseries_1',['bin_series',['../classbin__series.html',1,'bin_series&lt; T, K &gt;'],['../classbin__series.html#ad1ffb02d85904d979c420fcac63a9876',1,'bin_series::bin_series()']]],
-  ['binomial_5fcoefficient_2',['binomial_coefficient',['../classseries__base.html#ac4b76c3e457f15e689742687dc9f7d56',1,'series_base']]]
+  ['binomial_5fcoefficient_2',['binomial_coefficient',['../classseries__base.html#a9fa4e60a5c3f6455b9eab7117af2bc4d',1,'series_base']]]
 ];
--- search/all_5.js
+++ search/all_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['fact_12',['fact',['../classseries__base.html#abbb83c8b0ca71603bcfd2e9a456998c1',1,'series_base']]],
+  ['fact_12',['fact',['../classseries__base.html#ab2d22b51f0ff7368ce7b0f191b76850b',1,'series_base']]],
   ['four_5farctan_5fseries_13',['four_arctan_series',['../classfour__arctan__series.html',1,'four_arctan_series&lt; T, K &gt;'],['../classfour__arctan__series.html#a2516a73be1cfe8e830a543e0d63c5ae8',1,'four_arctan_series::four_arctan_series()']]]
 ];
