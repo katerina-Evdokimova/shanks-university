5fd21df5f7b4461aba515a7f2f557e44e0638d45
MikyMikyMiky <mike1024b@mail.ru>
2024-02-11 11:50:28 +0000

    Deploying to gh-pages from @ katerina-Evdokimova/shanks-university@8cea0af6493aed4fbf07e621945eaa9b75ced4d2 🚀

--- search/all_1.js
+++ search/all_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['bin_5fseries_1',['bin_series',['../classbin__series.html',1,'bin_series&lt; T, K &gt;'],['../classbin__series.html#ad1ffb02d85904d979c420fcac63a9876',1,'bin_series::bin_series()']]],
-  ['binomial_5fcoefficient_2',['binomial_coefficient',['../classseries__base.html#a9fa4e60a5c3f6455b9eab7117af2bc4d',1,'series_base']]]
+  ['binomial_5fcoefficient_2',['binomial_coefficient',['../classseries__base.html#a8e45cc09b6bdec85135cd1f05d5e444c',1,'series_base']]]
 ];
