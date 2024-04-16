3f3f46971288e6cf1bc6020807ad0530ca116bd4
MikyMikyMiky <mike1024b@mail.ru>
2024-04-16 06:36:40 +0000

    Deploying to gh-pages from @ katerina-Evdokimova/shanks-university@eede46e06650c11f01cc6fd8034f3ee073f07c1d 🚀

--- classfour__ln2__m__3__series.js
+++ classfour__ln2__m__3__series.js
@@ -1,5 +1,5 @@
 var classfour__ln2__m__3__series =
 [
     [ "four_ln2_m_3_series", "classfour__ln2__m__3__series.html#a3ab660fd63e33f02e4ab192d4353400e", null ],
-    [ "operator()", "classfour__ln2__m__3__series.html#a1fb59a0ae8ebd5e0a0314c513d08e0e9", null ]
+    [ "operator()", "classfour__ln2__m__3__series.html#a8c505d8d126baac85ca503e999ed0ae9", null ]
 ];
--- classthree__minus__pi__series.js
+++ classthree__minus__pi__series.js
@@ -1,5 +1,5 @@
 var classthree__minus__pi__series =
 [
     [ "three_minus_pi_series", "classthree__minus__pi__series.html#a678bd455c980592fbb407726d9a3c67e", null ],
-    [ "operator()", "classthree__minus__pi__series.html#a8a497709abdb34da27700b8186d6d2d5", null ]
+    [ "operator()", "classthree__minus__pi__series.html#af6020d1ffb42178f0d03af24a9085ea4", null ]
 ];
